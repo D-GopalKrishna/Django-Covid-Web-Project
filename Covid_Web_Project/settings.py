75642da482bba5f9eb4117f956ca35564491fcc0
D-GopalKrishna <nikhil0223@gmail.com>
2021-01-29 18:39:22 +0530

    Some adjusts

--- Covid_Web_Project/settings.py
+++ Covid_Web_Project/settings.py
@@ -30,7 +30,7 @@ SECRET_KEY = 'l=7azcq)uarml3$dah3y+&vqui$-yk7%#e)-w!eqkz9gg-5ta9'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['covid-web-project-django.herokuapp.com', '127.0.0.1']
+ALLOWED_HOSTS = ['covid-web-project.herokuapp.com', '127.0.0.1']
 
 
 # Application definition
